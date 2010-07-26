25d5e8eb5750f0800dee2425d286cba46511081e
Cyril Mougel <cyril.mougel@gmail.com>
2010-07-26 23:13:30 +0800

    avoid problem with big beta number in mongoid gem release

--- lib/matchers/document.rb
+++ lib/matchers/document.rb
@@ -6,15 +6,15 @@ module Mongoid
       end
 
       def of_type(type)
-        @type = type  
+        @type = type
         self
       end
-      
+
       def with_default_value_of(default)
         @default = default
         self
       end
-      
+
       def matches?(klass)
         @klass = klass.is_a?(Class) ? klass : klass.class
         @errors = []
@@ -24,11 +24,11 @@ module Mongoid
             if @type and @klass.fields[attr].type != @type
               error << " of type #{@klass.fields[attr].type}"
             end
-            
+
             if @default and @klass.fields[attr].default != @default
               error << " with default value of #{@klass.fields[attr].default}"
             end
-            
+
             @errors.push("field #{attr.inspect}" << error) unless error.blank?
           else
             @errors.push "no field named #{attr.inspect}"
@@ -52,42 +52,42 @@ module Mongoid
         desc
       end
     end
-    
+
     def have_field(*args)
       HaveFieldMatcher.new(*args)
     end
     alias_method :have_fields, :have_field
-    
+
     class SaveMatcher
       def initialize(attributes = {})
         @attributes = attributes
       end
-      
+
       def matches?(actual)
-        @actual = actual.is_a?(Class) ? 
+        @actual = actual.is_a?(Class) ?
           ( defined?(::Factory) ? ::Factory.build(actual.name.underscore, @attributes) : actual.new(@attributes)) :
           actual
         @actual.valid? and @actual.save
       end
-      
+
       def failure_message_for_should
         "Expected #{@actual.inspect} to save properly, got #{@actual.errors.full_messages.to_sentence}"
       end
-      
+
       def failure_message_for_should_not
         "Expected #{@actual.inspect} to not save, got saved instead"
       end
-      
+
       def description
         "save properly"
       end
     end
-    
+
     def save(attributes = {})
       SaveMatcher.new(attributes)
     end
-    alias_method :save_properly, :save    
-    
+    alias_method :save_properly, :save
+
   end
 end
 
@@ -95,7 +95,7 @@ RSpec::Matchers.define :have_instance_method do |name|
   match do |klass|
     klass.instance_methods.include?(name.to_sym)
   end
-  
+
   description do
     "have instance method #{name.to_s}"
   end
--- mongoid-rspec.gemspec
+++ mongoid-rspec.gemspec
@@ -9,7 +9,7 @@ Gem::Specification.new do |s|
 
   s.required_rubygems_version = Gem::Requirement.new(">= 0") if s.respond_to? :required_rubygems_version=
   s.authors = ["Evan Sagge"]
-  s.date = %q{2010-07-23}
+  s.date = %q{2010-07-26}
   s.description = %q{RSpec matches for Mongoid models, including association and validation matchers}
   s.email = %q{evansagge@gmail.com}
   s.extra_rdoc_files = [
@@ -67,16 +67,16 @@ Gem::Specification.new do |s|
     s.specification_version = 3
 
     if Gem::Version.new(Gem::VERSION) >= Gem::Version.new('1.2.0') then
-      s.add_runtime_dependency(%q<mongoid>, [">= 2.0.0.beta10"])
+      s.add_runtime_dependency(%q<mongoid>, ["~> 2.0.0"])
       s.add_runtime_dependency(%q<bson_ext>, [">= 0.20.1"])
       s.add_runtime_dependency(%q<rspec-rails>, [">= 2.0.0.beta.7"])
     else
-      s.add_dependency(%q<mongoid>, [">= 2.0.0.beta10"])
+      s.add_dependency(%q<mongoid>, ["~> 2.0.0"])
       s.add_dependency(%q<bson_ext>, [">= 0.20.1"])
       s.add_dependency(%q<rspec-rails>, [">= 2.0.0.beta.7"])
     end
   else
-    s.add_dependency(%q<mongoid>, [">= 2.0.0.beta10"])
+    s.add_dependency(%q<mongoid>, ["~> 2.0.0"])
     s.add_dependency(%q<bson_ext>, [">= 0.20.1"])
     s.add_dependency(%q<rspec-rails>, [">= 2.0.0.beta.7"])
   end
