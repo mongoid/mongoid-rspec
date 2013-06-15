1cf5f99d7055b4c0d9364160766620844fc055a6
Trung Lê <joneslee85@gmail.com>
2013-06-15 12:27:42 +1000

    Cut whitespaces

--- lib/matchers/validations/numericality_of.rb
+++ lib/matchers/validations/numericality_of.rb
@@ -2,14 +2,14 @@ module Mongoid
   module Matchers
     module Validations
       class ValidateNumericalityOfMatcher < HaveValidationMatcher
-        @@allowed_options = [:equal_to, :greater_than, :greater_than_or_equal_to, :less_than, :less_than_or_equal_to, 
+        @@allowed_options = [:equal_to, :greater_than, :greater_than_or_equal_to, :less_than, :less_than_or_equal_to,
           :even, :odd, :only_integer, :allow_nil, :nil]
-        
+
         def initialize(field)
           super(field, :numericality)
           @options = {}
         end
-        
+
         def to_allow(options)
           options[:equal_to] = options if options.is_a?(Numeric)
           options[:allow_nil] = options.delete(:nil) if options.has_key?(:nil)
@@ -21,7 +21,7 @@ module Mongoid
 
         def matches?(actual)
           return false unless result = super(actual)
-          
+
           @@allowed_options.each do |comparator|
             if @options.has_key?(comparator) and !([:even, :odd, :only_integer].include?(comparator) and !@validator.options.include?(comparator))
               result &= (@validator.options[comparator] == @options[comparator])
@@ -35,12 +35,12 @@ module Mongoid
         def description
           super << options_message(@options)
         end
-        
+
         protected
-        
+
         def options_message(options)
           type_msg = []
-          comp_msg = []          
+          comp_msg = []
           options.each_pair do |key, value|
             case key
             when :allow_nil
@@ -52,10 +52,10 @@ module Mongoid
               comp_msg << "#{key.to_s.gsub("_", " ")} #{value.inspect}"
             end
           end
-          allow_nil = (options[:allow_nil] ? "nil" : "non-nil") if options.has_key?(:allow_nil)          
+          allow_nil = (options[:allow_nil] ? "nil" : "non-nil") if options.has_key?(:allow_nil)
           ["", "allowing", allow_nil, type_msg.any? ? type_msg.to_sentence : nil, "values", comp_msg.any? ? comp_msg.to_sentence : nil].compact.join(" ")
-        end        
-        
+        end
+
         def method_missing(m, *args, &block)
           if @@allowed_options.include?(m.to_sym)
             raise ArgumentError, "wrong number of arguments (#{args.length} for 1)" if args.length > 1
