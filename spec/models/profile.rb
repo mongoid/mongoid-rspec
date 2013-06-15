ded995667f4b471b85f245be71999ce4ef957b19
Trung Lê <joneslee85@gmail.com>
2013-06-15 12:34:48 +1000

    Refactor spec/models:
    * Use the sexy validation syntax of mongoid
    * Get rid of ruby1.8 hash syntax

--- spec/models/profile.rb
+++ spec/models/profile.rb
@@ -1,16 +1,16 @@
 class Profile
   include Mongoid::Document
-  
+
   field :first_name
   field :last_name
   field :age
   field :hobbies, type: Array, default: []
 
-  embedded_in :user, :inverse_of => :profile
-  
-  validates_numericality_of :age, :greater_than => 0
-  validates_acceptance_of :terms_of_service
-  validates_length_of :hobbies, minimum: 1, message: "requires at least one hobby"
+  embedded_in :user, inverse_of: :profile
 
-  index({first_name: 1, last_name: 1 })
+  validates :age, numericality: { greater_than: 0 }
+  validates :terms_of_service, acceptance: true
+  validates :hobbies, length: { minimum: 1, message: "requires at least one hobby" }
+
+  index({ first_name: 1, last_name: 1 })
 end
